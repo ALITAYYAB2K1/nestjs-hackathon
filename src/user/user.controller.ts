f37d9660253c15dede97e88b7c56b4cbc92fe700
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2026-08-02 22:23:06 +0500

    querry decorators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get('all')
+  getUsers(@Query('name') name: string) {
+    const users = [
+      { id: 1, name: 'Zeeshan' },
+      { id: 2, name: 'Ali' },
+      { id: 3, name: 'Ahmed' },
+    ];
+
+    if (name) {
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(name.toLowerCase()),
+      );
+    }
+
+    return users;
+  }
+}
